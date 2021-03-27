180f729bee006e6682fecd297b1395a925a219a4
Rochan2001 <bahadur.rochan@gmail.com>
2021-03-27 15:15:14 +0530

    added sync

--- public/syncview.js
+++ public/syncview.js
@@ -1,15 +1,22 @@
-var socket = io.connect("http://localhost:8000");
-
-var viewer = OpenSeadragon({
+var socket = io.connect("172.16.20.50:8000");
+// var socket = io.connect("http://localhost:8000");
+var viewer1 = OpenSeadragon({
   id: "openseadragon1",
   prefixUrl: "../openseadragon/images/",
   tileSources: "../image/mydz.dzi",
   visibilityRatio: 1.0,
   constrainDuringPan: true,
 });
+var viewer2 = OpenSeadragon({
+  id: "openseadragon2",
+  prefixUrl: "../openseadragon/images/",
+  tileSources: "../image/mydz.dzi",
+  visibilityRatio: 1.0,
+  constrainDuringPan: true,
+});
 
-viewer.addHandler("canvas-drag", function () {
-  const center = viewer.viewport.getCenter();
+viewer2.addHandler("canvas-drag", function () {
+  const center = viewer2.viewport.getCenter();
 
   var data = {
     center: center,
@@ -18,9 +25,9 @@ viewer.addHandler("canvas-drag", function () {
   socket.emit("message", data);
 });
 
-viewer.addHandler("canvas-scroll", function () {
-  const zoom = viewer.viewport.getZoom();
-  const center = viewer.viewport.getCenter();
+viewer2.addHandler("canvas-scroll", function () {
+  const zoom = viewer2.viewport.getZoom();
+  const center = viewer2.viewport.getCenter();
 
   var data = {
     zoom: zoom,
@@ -37,13 +44,13 @@ socket.on("message", function (data) {
     data.hasOwnProperty("center") &&
     !data.hasOwnProperty("zoom")
   ) {
-    viewer.viewport.panTo(data.center);
+    viewer2.viewport.panTo(data.center);
   } else if (
     data.type === "event" &&
     socket.id !== data.socketId &&
     data.hasOwnProperty("zoom")
   ) {
-    viewer.viewport.zoomTo(data.zoom);
-    viewer.viewport.panTo(data.center);
+    viewer2.viewport.zoomTo(data.zoom);
+    viewer2.viewport.panTo(data.center);
   }
 });
