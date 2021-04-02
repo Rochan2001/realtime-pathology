c35fa80ec1c9e89f2a9722dfe1797b46166a3080
B.ROCHAN SINGH <bahadur.rochan@gmail.com>
2021-04-02 13:38:13 +0530

    syncronized and added ID's

--- public/syncview.js
+++ public/syncview.js
@@ -1,5 +1,7 @@
 // var socket = io.connect("172.16.20.50:8000");
 var socket = io.connect("http://localhost:8000");
+var temp = false;
+
 var viewer1 = OpenSeadragon({
   id: "openseadragon1",
   prefixUrl: "../openseadragon/images/",
@@ -15,8 +17,12 @@ var viewer2 = OpenSeadragon({
   constrainDuringPan: true,
 });
 
-viewer2.addHandler("canvas-drag", function () {
-  const center = viewer2.viewport.getCenter();
+socket.on("connect", function () {
+  document.getElementById("doc1").innerHTML = "docId: " + socket.id.toString();
+});
+
+viewer1.addHandler("canvas-drag", function () {
+  const center = viewer1.viewport.getCenter();
 
   var data = {
     center: center,
@@ -25,9 +31,9 @@ viewer2.addHandler("canvas-drag", function () {
   socket.emit("message", data);
 });
 
-viewer2.addHandler("canvas-scroll", function () {
-  const zoom = viewer2.viewport.getZoom();
-  const center = viewer2.viewport.getCenter();
+viewer1.addHandler("canvas-scroll", function () {
+  const zoom = viewer1.viewport.getZoom();
+  const center = viewer1.viewport.getCenter();
 
   var data = {
     zoom: zoom,
@@ -38,19 +44,21 @@ viewer2.addHandler("canvas-scroll", function () {
 
 socket.on("message", function (data) {
   console.log(socket.id + " " + data.socketId);
-  if (
-    data.type === "event" &&
-    socket.id !== data.socketId &&
-    data.hasOwnProperty("center") &&
-    !data.hasOwnProperty("zoom")
-  ) {
-    viewer2.viewport.panTo(data.center);
-  } else if (
-    data.type === "event" &&
-    socket.id !== data.socketId &&
-    data.hasOwnProperty("zoom")
-  ) {
-    viewer2.viewport.zoomTo(data.zoom);
-    viewer2.viewport.panTo(data.center);
+  if (socket.id !== data.socketId) {
+    if (temp === false) {
+      document.getElementById("doc2").innerHTML =
+        "docId: " + data.socketId.toString();
+      temp = true;
+    }
+    if (
+      data.type === "event" &&
+      data.hasOwnProperty("center") &&
+      !data.hasOwnProperty("zoom")
+    ) {
+      viewer2.viewport.panTo(data.center);
+    } else if (data.type === "event" && data.hasOwnProperty("zoom")) {
+      viewer2.viewport.zoomTo(data.zoom);
+      viewer2.viewport.panTo(data.center);
+    }
   }
 });
